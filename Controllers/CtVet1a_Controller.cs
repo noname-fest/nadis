fad291b5284ae75c4c1161bd0101ef2d0ec24eec
avtan <smankulovavtan@gmail.com>
2020-03-10 20:41:09 +0600

    add description for CtVet1a

---
+++ Controllers/CtVet1a_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace nadis.Controllers
+{
+    public class CtVet1a_Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
