a59b70823609c7f48a4bf5073d1c1bcbd3420e72
avtan <smankulovavtan@gmail.com>
2020-03-11 00:44:01 +0600

    day 1

---
+++ Controllers/CtVet1aController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using nadis.Models;
+
+namespace nadis.Controllers
+{
+    public class CtVet1aController : Controller
+    {
+        readonly CtVet1aDAL vet1aDAL = new CtVet1aDAL();
+        public IActionResult Index()
+        {
+            _ = new List<CtVet1a>();
+            List<CtVet1a> vet1aList = vet1aDAL.GetAllCtVet1a("RD04235", "01/12/2019").ToList();
+            return View(vet1aList);
+        }
+    }
+}
---
+++ Models/CtVet1aDAL.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace nadis.Models
+{
+    public class CtVet1aDAL
+    {
+        readonly string connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=nadis;Integrated Security=True";
+
+        public IEnumerable<CtVet1a> GetAllCtVet1a(string KIDro, string repMO)
+        {
+            List<CtVet1a> tmpList = new List<CtVet1a>();
+            using(SqlConnection _conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Get_CtVet1a", _conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddWithValue("@KIDro", KIDro);
+                cmd.Parameters.AddWithValue("@repMO", repMO);
+                _conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    CtVet1a tmp = new CtVet1a
+                    {
+                        ID = Guid.Parse(dr["ID"].ToString()),
+                        GID = dr["GID"].ToString(),
+                        RepMO = (DateTime)dr["repMO"],
+                        KIDdiv = dr["KIDdiv"].ToString(),
+                        KIDspc = dr["KIDspc"].ToString()
+                    };
+
+                    tmpList.Add(tmp);
+                }
+                _conn.Close();
+            }
+            return tmpList;
+        }
+
+        public void AddCtVet1a(CtVet1a tmp)
+        {
+            /*
+            using (SqlConnection _conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("sp_Add_CtVet1a", _conn)
+                {
+                    CommandType = CommandType.StoredProcedure
+                };
+                cmd.Parameters.AddWithValue("@KIDdiv", tmp.KIDdiv);
+                cmd.Parameters.AddWithValue("@repMO", tmp.RepMO);
+
+                _conn.Open();
+                cmd.ExecuteNonQuery();
+                _conn.Close();
+            }
+            */
+
+            using SqlConnection _conn = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand("sp_Add_CtVet1a", _conn)
+            {
+                CommandType = CommandType.StoredProcedure
+            };
+            cmd.Parameters.AddWithValue("@KIDdiv", tmp.KIDdiv);
+            cmd.Parameters.AddWithValue("@repMO", tmp.RepMO);
+
+            _conn.Open();
+            cmd.ExecuteNonQuery();
+            _conn.Close();
+        }
+    }
+}
